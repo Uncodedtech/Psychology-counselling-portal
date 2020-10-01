a4072ae390fedd6bc4b99b941b28d6f2aaf4d702
Vsrj <71036208+vsrjav@users.noreply.github.com>
2020-10-01 07:05:06 +0530

    added public

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const bodyParser = require("body-parser");
 
 const data_items =[""];
 
+
+
+
+app.use(express.static("public"));
 app.listen(4000, function() {
   console.log("Server running");
 });
